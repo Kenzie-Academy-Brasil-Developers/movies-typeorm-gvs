5a829e44c59ac3fe9492e51b4244c1b85bba7cef
gusmfs <oigostavo@gmail.com>
2023-10-20 16:38:47 -0300

    gs

---
+++ src/middlewares/handleError.middleware.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+import AppError from "../errors/AppError.error";
+
+export const handdleErrors = (err: unknown ,req: Request, res: Response, next : NextFunction) : Response => {
+    if(err instanceof AppError){
+        return res.status(err.status).json({message : err.message});
+    }
+    console.log(err)
+    return res.status(500).json({message : 'Internal Server Error'});
+}
