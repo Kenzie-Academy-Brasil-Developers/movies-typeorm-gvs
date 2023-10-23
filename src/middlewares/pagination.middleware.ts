143963e1d00e1cd9ca962fa0e2f8160dda94ef62
gusmfs <oigostavo@gmail.com>
2023-10-23 16:41:40 -0300

    aads

--- src/middlewares/pagination.middleware.ts
+++ src/middlewares/pagination.middleware.ts
@@ -4,7 +4,18 @@ export const pagination = (req: Request, res: Response , next: NextFunction) : v
     const queryPage : number = Number(req.query.page)
     const queryPerPage : number = Number(req.query.perPage)
 
-    const page : number = queryPage && queryPage > 1 ? queryPage : 1
+    const page : number = queryPage && queryPage >= 1 ? queryPage : 1
     const perPage : number = queryPerPage && queryPerPage <= 5 && queryPage > 0 ? queryPerPage : 5
 
+
+    const baseUrl : string = 'http://localhost:3000/movies'
+    const prevPage : string = `${baseUrl}?page=${page - 1}&perPage=${perPage}`
+    const nextPage : string = `${baseUrl}?page=${page + 1}&perPage=${perPage}`
+
+    const pagination = {
+        page, perPage, prevPage, nextPage
+    }
+    res.locals = {...res.locals, pagination}
+
+    return next()
 }
