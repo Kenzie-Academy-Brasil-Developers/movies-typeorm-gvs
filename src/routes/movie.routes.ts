e2154ec2bd27e67c558984edf767bd9a6d502ca4
gusmfs <oigostavo@gmail.com>
2023-10-23 15:13:23 -0300

    aa

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createMovieController, deleteMovieController, readMovieController, updateMovieController } from "../controller/movies.controller";
 import { verifyIdExist } from "../middlewares/verifyIdExist.middleware";
+import { validateBody } from "../middlewares/validateBody.middleware";
+import { movieCreateSchema } from "../schemas/movies.schemas";
 
 export const movieRoutes : Router = Router();
 movieRoutes.use('/:id', verifyIdExist)
-movieRoutes.post('/', createMovieController)
+movieRoutes.post('/', validateBody(movieCreateSchema),createMovieController)
 movieRoutes.get('/', readMovieController)
 movieRoutes.patch('/:id', updateMovieController)
 movieRoutes.delete('/id', deleteMovieController) 
