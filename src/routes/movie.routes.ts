312458325824882594a898a2652e2e23cb36bf88
gusmfs <oigostavo@gmail.com>
2023-10-25 01:18:04 -0300

    dasd

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 import { createMovieController, deleteMovieController, readMovieController, updateMovieController } from "../controller/movies.controller";
 import { verifyIdExist } from "../middlewares/verifyIdExist.middleware";
 import { validateBody } from "../middlewares/validateBody.middleware";
-import { movieCreateSchema } from "../schemas/movies.schemas";
+import { movieCreateSchema, movieUpdateSchema } from "../schemas/movies.schemas";
 import { pagination } from "../middlewares/pagination.middleware";
 
 export const movieRoutes : Router = Router();
 movieRoutes.use('/:id', verifyIdExist)
 movieRoutes.post('/', validateBody(movieCreateSchema),createMovieController)
 movieRoutes.get('/', pagination ,readMovieController)
-movieRoutes.patch('/:id', updateMovieController)
+movieRoutes.patch('/:id', validateBody(movieUpdateSchema),updateMovieController)
 movieRoutes.delete('/id', deleteMovieController) 
