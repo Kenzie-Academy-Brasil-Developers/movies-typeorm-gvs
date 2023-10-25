eed7224b70a24eaa6baaec4bce817a716d060480
gusmfs <oigostavo@gmail.com>
2023-10-25 19:36:27 -0300

    asdas

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { handdleErrors } from "./middlewares/handleError.middleware"
 import { movieRoutes } from "./routes/movie.routes"
 //import {routes} from "./routes"
 //import {handdleErrors} from "./middlewares/handdleErrors.middlewares
-const app : Application = express()
+export const app : Application = express()
 app.use(json())
 app.use('/movies', movieRoutes)
 app.use(handdleErrors)
